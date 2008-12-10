466b9e6316e2ef133feb9db935566d229eb84c31
Benny Wong <bdotdub@gmail.com>
2008-12-10 00:34:49 -0500

    altered mushroom to have more class-like features rudimentary playlist creation spore skeleton, doesn't seem to be working yet

--- javascripts/mushroom.js
+++ javascripts/mushroom.js
@@ -1,3 +1,8 @@
+
+// Modules/classes, namely $Spore and $Mushroom are modeled after
+// Douglas Crockford module pattern. Read more about it here:
+//  http://yuiblog.com/blog/2007/06/12/module-pattern/
+
 (function($) {
   $.fn.mushroom = function(options) {
     var defaults = {
@@ -14,24 +19,73 @@
       new $Mushroom(this, options);
     });
   };
+  
+  $Spore = function(soundObj, prev, next) {
+    this.soundObj = soundObj;
+    this.prev     = prev;
+    this.next     = 'null!!';
+    
+    this.playSong = function() {
+      console.log('hello!' + this.soundObj.sID)
+    }
+    
+    return {
+      play: this.playSong
+    };
+  }
 
-  function mushroomify(playerElement, options) {
-    $('ul.playlist li', playerElement).click(function() {
-      var soundElement = $('a', this);
-      if (soundElement.length != 0) {
+  $Mushroom = function(playerElement, options) {
+    // Properties
+    this.playlist           = null;
+    this.currentlyPlaying   = null;
+    
+    // Alias this to self
+    self = this;
+    
+    // This goes through each of the items in the list and
+    // sets up the sound for it
+    this.addPlaylistSongs = function() {
+      $('ul.playlist li', playerElement).each(function() {
+        // Get the anchor with the URL
+        var soundElement = $('a', this);
+        if (soundElement.length == 0) {
+          console.warn('No suitable song for this list item');
+          return;
+        }
+        
+        // Create the SoundManager sound
         var soundUrl = soundElement[0].href;
 				thisSound = soundManager.createSound({
 				  id:soundUrl,
 				  url:soundUrl
 				});
-
-        soundManager.stopAll();
-        thisSound.play();
-      }
-      else {
-        // Log and let them know it should only on link per element
-      }
-      return false;
-    });
+				
+				// Get the playlist so we can find prev/next
+				if (self.playlist == null) { self.playlist = new Array() }
+				
+				// Find the prev, if any.
+				var prev = null;
+				if (self.playlist.length > 0) {
+				  var prevIndex = self.playlist.length;
+				  prev = self.playlist[prevIndex];
+				}
+				
+				// Create a new Spore
+				var spore = new $Spore(thisSound, prev);
+				
+				self.playlist.push(spore);
+      });
+    };
+    
+    this.hookUpControls = function() {
+      
+    };
+    
+    // Do stuff!
+    this.addPlaylistSongs();
+    this.hookUpControls();
+    
+    console.log('blah! ' + this.playlist.length);
+    this.playlist[0].play();
   }
 })(jQuery);
