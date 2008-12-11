2a411ba8bb7d31d9ccd0cb58b862ff950fa6c237
Benny Wong <bdotdub@gmail.com>
2008-12-11 00:11:56 -0500

    added repeat option added more functionality to spore and made it working extract out the sound object creation to spore

--- javascripts/mushroom.js
+++ javascripts/mushroom.js
@@ -6,7 +6,7 @@
 (function($) {
   $.fn.mushroom = function(options) {
     var defaults = {
-      
+      repeat: true
     };
 
     // Merge defaults and options
@@ -20,32 +20,84 @@
     });
   };
   
-  $Spore = function(soundObj, prev, next) {
-    this.soundObj = soundObj;
-    this.prev     = prev;
-    this.next     = 'null!!';
+  $Spore = function(parent, soundElement, soundUrl, prevSong, nextSong) {
+    // Properties
+    this.element  = soundElement;
+    this.prevSong = prevSong;
+    this.nextSong = null;
+    this.soundObj = null;
+    this.url      = soundUrl;
+    this.mushroom = parent;
     
-    this.playSong = function() {
-      console.log('hello!' + this.soundObj.sID)
+    // Alias this to self
+    var self = this;
+    
+    // Play this!
+    this.play = function() {
+      self.soundObj.play();
+      self.element.parent().addClass('playing');
+    };
+    
+    // Stop it
+    this.stop = function() {
+      self.element.parent().removeClass('playing');
+      self.soundObj.stop();
     }
     
-    return {
-      play: this.playSong
+    // Stop it
+    this.pause = function() {
+      self.soundObj.pause();
+    }
+    
+    // Click handler
+    this.click = function() {
+      self.mushroom.currentlyPlaying.stop();
+      
+      // Set a currentlyPlaying
+      self.mushroom.currentlyPlaying = self;
+      
+      // Stop and then play
+      soundManager.stopAll();
+      self.play();
+      
+      // Stop the click
+      return false;
     };
-  }
+    
+    this.init = function() {
+      // Set up the links
+      var prevSong = self.prevSong;
+      if (prevSong != null) {
+        prevSong.nextSong = self;
+      }
+      
+      // Attach the click handler
+      self.element.click(self.click);
+      
+      // Create the sound object
+      self.soundObj = soundManager.createSound({
+			  id:self.url,
+			  url:self.url,
+			  onfinish: self.mushroom.goToNextSong
+			});
+    };
+    
+    this.init();
+  };
 
   $Mushroom = function(playerElement, options) {
     // Properties
     this.playlist           = null;
     this.currentlyPlaying   = null;
+    this.options            = options;
     
     // Alias this to self
-    self = this;
+    var self = this;
     
     // This goes through each of the items in the list and
     // sets up the sound for it
     this.addPlaylistSongs = function() {
-      $('ul.playlist li', playerElement).each(function() {
+      $('ul.playlist li', self.playerElement).each(function() {
         // Get the anchor with the URL
         var soundElement = $('a', this);
         if (soundElement.length == 0) {
@@ -53,39 +105,73 @@
           return;
         }
         
-        // Create the SoundManager sound
-        var soundUrl = soundElement[0].href;
-				thisSound = soundManager.createSound({
-				  id:soundUrl,
-				  url:soundUrl
-				});
-				
-				// Get the playlist so we can find prev/next
+        // Get the playlist so we can find prev/next
 				if (self.playlist == null) { self.playlist = new Array() }
 				
 				// Find the prev, if any.
 				var prev = null;
 				if (self.playlist.length > 0) {
-				  var prevIndex = self.playlist.length;
+				  var prevIndex = self.playlist.length - 1;
 				  prev = self.playlist[prevIndex];
 				}
 				
-				// Create a new Spore
-				var spore = new $Spore(thisSound, prev);
+				// Get the song URL
+        var soundUrl = soundElement[0].href;
 				
+				// Create a new Spore
+				var spore = new $Spore(self, soundElement, soundUrl, prev);
 				self.playlist.push(spore);
       });
+      
+      // Set initial current song
+      if (self.playlist.length > 0) {
+        self.currentlyPlaying = self.playlist[0];
+      }
     };
     
     this.hookUpControls = function() {
+      // Setup play button
+      $('.play', self.playerElement).click(function() {
+        if (!playlistHasSongs) { return; }
+        self.currentlyPlaying.play();
+      });
       
+      // Setup stop button
+      $('.stop', self.playerElement).click(function() {
+        if (!playlistHasSongs) { return; }
+        self.currentlyPlaying.stop();
+      });
+      
+      // Setup play button
+      $('.pause', self.playerElement).click(function() {
+        if (!playlistHasSongs) { return; }
+        self.currentlyPlaying.pause();
+      });
+    };
+    
+    function playlistHasSongs() {
+      return (self.playlist.length > 0);
+    }
+    
+    // Sound object handlers
+    this.goToNextSong = function() {
+      self.currentlyPlaying.stop();
+      self.currentlyPlaying = self.currentlyPlaying.nextSong;
+
+      if (self.currentlyPlaying == null && self.options.repeat) {
+        self.currentlyPlaying = self.playlist[0];
+      }
+      
+      if (self.currentlyPlaying != null) {
+        self.currentlyPlaying.play();
+      }
     };
     
     // Do stuff!
     this.addPlaylistSongs();
     this.hookUpControls();
     
-    console.log('blah! ' + this.playlist.length);
-    this.playlist[0].play();
-  }
+    console.log(this);
+  };
+  
 })(jQuery);
